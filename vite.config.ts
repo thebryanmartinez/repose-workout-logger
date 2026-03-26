f2c27da4a87e7380ecc02cb3c003fd8ed832fa22
Bryan Martinez <bryanmartz972@gmail.com>
2026-03-26 12:39:43 -0600

    init commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite-plus";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+
+// https://vite.dev/config/
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  lint: { options: { typeAware: true, typeCheck: true } },
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+});
